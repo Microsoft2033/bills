42f3db825deaec46718d037c2578d81a51d46df7
Dauda Abdulhakeem <daudahakeeme1@gmail.com>
2024-10-09 09:44:50 -0300

    ui design

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -2,6 +2,9 @@ export  default function Dashboard(){
     return(
         <main>
             <p>user Dashboard</p>
+            <p>propose a ui design</p>
+            <p>build the ui</p>
+            <p>propose possible changes</p>
         </main>
     )
 }
