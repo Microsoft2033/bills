ce4d7ca5cc2ec9694a8dfb75e3f3d65c727c41cb
Dauda Abdulhakeem <daudahakeeme1@gmail.com>
2024-10-14 07:52:04 -0300

    history data

---
+++ app/dashboard/history/page.js
@@ -0,0 +1,18 @@
+import { HistoryTab } from "@/components/HiatoryTab"
+
+// delete this later
+const dummy = [1, 2, 3, 4];
+export default function History() {
+    return (
+        <main className="min-h-screen flex justify-center items-center bg-gradient-to-b from-sky-100 via-sky-200 to-blue-300">
+            <div className="w-[380px] min-h-[400px] bg-white rounded-md p-4">
+                <h1 className="text-xl text-gray-800 mb-4">My loan History</h1>
+                <div className="flex flex-col gap-4">
+                    {/* <HistoryTab/> */}
+                    {dummy.map(loan => <HistoryTab amount={5000} rate={12.5} duration={30} type="personal" date="13 Jul 2024" key={loan}/>)}
+
+                </div>
+            </div>
+        </main>
+    )
+}
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export  default function Dashboard(){
     return(
         <main>
@@ -5,6 +6,7 @@ export  default function Dashboard(){
             <p>propose a ui design</p>
             <p>build the ui</p>
             <p>propose possible changes</p>
+            <Image width={480} height={250} src="/coff1.jpg" alt="screenshot"/>
         </main>
     )
 }
